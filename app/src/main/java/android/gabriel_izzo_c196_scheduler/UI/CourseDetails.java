ae0dda715aa9157e3563d2499cf0180b728aea39
Gabriel Izzo <52051491+gabeizzo@users.noreply.github.com>
2023-01-03 02:04:39 -0800

    1/3/2023 end of run

--- app/src/main/java/android/gabriel_izzo_c196_scheduler/UI/CourseDetails.java
+++ app/src/main/java/android/gabriel_izzo_c196_scheduler/UI/CourseDetails.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -34,28 +35,27 @@ public class CourseDetails extends AppCompatActivity {
     Repository repo;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_details);
-        courseTitle=findViewById(R.id.courseTitleTxt);
-        courseStart=findViewById(R.id.courseStartTxt);
-        courseEnd=findViewById(R.id.courseEndTxt);
-        courseStatus=findViewById(R.id.courseStatusTxt);
-        instructorName=findViewById(R.id.instructorNameTxt);
-        instructorPhone=findViewById(R.id.instructorPhoneTxt);
-        instructorEmail=findViewById(R.id.instructorEmailTxt);
-        courseNotes=findViewById(R.id.courseNotesTxt);
-        courseID=getIntent().getIntExtra("id", -1);
-        title=getIntent().getStringExtra("title");
-        start=getIntent().getStringExtra("start");
-        end=getIntent().getStringExtra("end");
-        status=getIntent().getStringExtra("status");
-        name=getIntent().getStringExtra("instructor");
-        phone=getIntent().getStringExtra("phone");
-        email=getIntent().getStringExtra("email");
-        notes=getIntent().getStringExtra("notes");
+        courseTitle = findViewById(R.id.courseTitleTxt);
+        courseStart = findViewById(R.id.courseStartTxt);
+        courseEnd = findViewById(R.id.courseEndTxt);
+        courseStatus = findViewById(R.id.courseStatusTxt);
+        instructorName = findViewById(R.id.instructorNameTxt);
+        instructorPhone = findViewById(R.id.instructorPhoneTxt);
+        instructorEmail = findViewById(R.id.instructorEmailTxt);
+        courseNotes = findViewById(R.id.courseNotesTxt);
+        courseID = getIntent().getIntExtra("id", -1);
+        title = getIntent().getStringExtra("title");
+        start = getIntent().getStringExtra("start");
+        end = getIntent().getStringExtra("end");
+        status = getIntent().getStringExtra("status");
+        name = getIntent().getStringExtra("instructor");
+        phone = getIntent().getStringExtra("phone");
+        email = getIntent().getStringExtra("email");
+        notes = getIntent().getStringExtra("notes");
         courseTitle.setText(title);
         courseStart.setText(start);
         courseEnd.setText(end);
@@ -66,16 +66,18 @@ public class CourseDetails extends AppCompatActivity {
         courseNotes.setText(notes);
 
 
-        repo=new Repository(getApplication());
+        repo = new Repository(getApplication());
     }
+
     @Override
-    public boolean onCreateOptionsMenu (Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         //Inflate the menu; this adds items to the action bar if it is present
         getMenuInflater().inflate(R.menu.menu_course_details, menu);
         return true;
     }
+
     @Override
-    public boolean onOptionsItemSelected (MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 this.finish();
@@ -84,16 +86,15 @@ public class CourseDetails extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void saveCourse(View view)   {
+    public void saveCourse(View view) {
         Course course;
-        if (courseID == -1){
-            int newID = repo.getAllCourses().get(repo.getAllCourses().size()-1).getCourseID()+1;
+        if (courseID == -1) {
+            int newID = repo.getAllCourses().get(repo.getAllCourses().size() - 1).getCourseID() + 1;
             course = new Course(newID, courseTitle.getText().toString(), courseStart.getText().toString(),
                     courseEnd.getText().toString(), courseStatus.getText().toString(), instructorName.getText().toString(),
                     instructorPhone.getText().toString(), instructorEmail.getText().toString(), courseNotes.getText().toString());
             repo.insert(course);
-        }
-        else {
+        } else {
             course = new Course(courseID, courseTitle.getText().toString(), courseStart.getText().toString(),
                     courseEnd.getText().toString(), courseStatus.getText().toString(), instructorName.getText().toString(),
                     instructorPhone.getText().toString(), instructorEmail.getText().toString(), courseNotes.getText().toString());
@@ -102,15 +103,24 @@ public class CourseDetails extends AppCompatActivity {
     }
 
     public void goToTerms(MenuItem item) {
-        Intent intent=new Intent(CourseDetails.this, TermList.class);
+        Intent intent = new Intent(CourseDetails.this, TermList.class);
         startActivity(intent);
     }
 
-    public void goToAssessments(MenuItem item){
-        Intent intent=new Intent(CourseDetails.this, AssessmentList.class);
+    public void goToAssessments(MenuItem item) {
+        Intent intent = new Intent(CourseDetails.this, AssessmentList.class);
         startActivity(intent);
     }
 
     public void shareCourseDetails(MenuItem item) {
+        EditText noteToShare = findViewById(R.id.courseNotesTxt);
+        String message = noteToShare.getText().toString();
+        Intent email = new Intent(Intent.ACTION_SEND);
+        email.putExtra(Intent.EXTRA_TEXT, message);
+
+        //need this to prompts email client only
+        email.setType("message/rfc822");
+
+        startActivity(Intent.createChooser(email, "Choose an Email client :"));
     }
 }
